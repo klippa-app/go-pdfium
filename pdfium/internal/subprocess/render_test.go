f5b83f1f7e7647d6d87c76fb75f43d3204f4adb6
Jeroen Bobbeldijk <jeroen@klippa.com>
2021-08-25 17:15:58 +0200

    Render tests

--- pdfium/internal/subprocess/render_test.go
+++ pdfium/internal/subprocess/render_test.go
@@ -3,12 +3,11 @@ package subprocess_test
 import (
 	"bytes"
 	"encoding/gob"
-	"image"
-	"io/ioutil"
-
 	"github.com/klippa-app/go-pdfium/pdfium/internal/subprocess"
 	"github.com/klippa-app/go-pdfium/pdfium/requests"
 	"github.com/klippa-app/go-pdfium/pdfium/responses"
+	"image"
+	"io/ioutil"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -106,22 +105,10 @@ var _ = Describe("Render", func() {
 								DPI:  100,
 							})
 							Expect(err).To(BeNil())
-
-							preRender, err := ioutil.ReadFile("./testdata/render_testpdf_dpi_100.gob")
-							Expect(err).To(BeNil())
-
-							buf := bytes.NewBuffer(preRender)
-							dec := gob.NewDecoder(buf)
-
-							var preRenderImage image.RGBA
-							err = dec.Decode(&preRenderImage)
-							Expect(err).To(BeNil())
-
 							Expect(renderedPage).To(Equal(&responses.RenderPage{
-								Image:             &preRenderImage,
+								Image:             loadPrerenderedImage("./testdata/render_testpdf_dpi_100.gob"),
 								PointToPixelRatio: 1.3888888888888888,
 							}))
-
 							Expect(renderedPage.Image.Bounds().Size().X).To(Equal(827))
 							Expect(renderedPage.Image.Bounds().Size().Y).To(Equal(1170))
 						})
@@ -134,18 +121,8 @@ var _ = Describe("Render", func() {
 								DPI:  300,
 							})
 							Expect(err).To(BeNil())
-
-							preRender, err := ioutil.ReadFile("./testdata/render_testpdf_dpi_300.gob")
-							Expect(err).To(BeNil())
-
-							buf := bytes.NewBuffer(preRender)
-							dec := gob.NewDecoder(buf)
-
-							var preRenderImage image.RGBA
-							err = dec.Decode(&preRenderImage)
-							Expect(err).To(BeNil())
 							Expect(renderedPage).To(Equal(&responses.RenderPage{
-								Image:             &preRenderImage,
+								Image:             loadPrerenderedImage("./testdata/render_testpdf_dpi_300.gob"),
 								PointToPixelRatio: 4.166666666666667,
 							}))
 
@@ -172,80 +149,88 @@ var _ = Describe("Render", func() {
 								Page:  0,
 								Width: 2000,
 							})
+
 							Expect(err).To(BeNil())
 							Expect(renderedPage).To(Equal(&responses.RenderPage{
-								PointToPixelRatio: 4.166666666666667,
+								Image:             loadPrerenderedImage("./testdata/render_testpdf_pixels_2000x0.gob"),
+								PointToPixelRatio: 3.3597884547259587,
 							}))
-
-							Expect(renderedPage.Image.Bounds().Size().X).To(Equal(827))
-							Expect(renderedPage.Image.Bounds().Size().Y).To(Equal(1170))
+							Expect(renderedPage.Image.Bounds().Size().X).To(Equal(2000))
+							Expect(renderedPage.Image.Bounds().Size().Y).To(Equal(2829))
 						})
 					})
 
 					Context("with only the height given", func() {
 						It("returns the right image, point to pixel ratio and resolution", func() {
-							pageSize, err := pdfium.RenderPageInPixels(&requests.RenderPageInPixels{
+							renderedPage, err := pdfium.RenderPageInPixels(&requests.RenderPageInPixels{
 								Page:   0,
 								Height: 2000,
 							})
-							Expect(err).To(BeNil())
-							Expect(pageSize).To(Equal(&responses.RenderPage{
-								PointToPixelRatio: 4.166666666666667,
-							}))
 
-							Expect(pageSize.Image.Bounds().Size().X).To(Equal(827))
-							Expect(pageSize.Image.Bounds().Size().Y).To(Equal(1170))
+							Expect(err).To(BeNil())
+							Expect(renderedPage).To(Equal(&responses.RenderPage{
+								Image:             loadPrerenderedImage("./testdata/render_testpdf_pixels_0x2000.gob"),
+								PointToPixelRatio: 2.375608084404265,
+							}))
+							Expect(renderedPage.Image.Bounds().Size().X).To(Equal(1415))
+							Expect(renderedPage.Image.Bounds().Size().Y).To(Equal(2000))
 						})
 					})
 
 					Context("with both the width and height given", func() {
 						Context("and the width and height being equal", func() {
 							It("returns the right image, point to pixel ratio and resolution", func() {
-								pageSize, err := pdfium.RenderPageInPixels(&requests.RenderPageInPixels{
+								renderedPage, err := pdfium.RenderPageInPixels(&requests.RenderPageInPixels{
 									Page:   0,
 									Width:  2000,
 									Height: 2000,
 								})
+
 								Expect(err).To(BeNil())
-								Expect(pageSize).To(Equal(&responses.RenderPage{
-									PointToPixelRatio: 4.166666666666667,
+								Expect(renderedPage).To(Equal(&responses.RenderPage{
+									Image:             loadPrerenderedImage("./testdata/render_testpdf_pixels_2000x2000.gob"),
+									PointToPixelRatio: 2.375608084404265,
 								}))
 
-								Expect(pageSize.Image.Bounds().Size().X).To(Equal(827))
-								Expect(pageSize.Image.Bounds().Size().Y).To(Equal(1170))
+								Expect(renderedPage.Image.Bounds().Size().X).To(Equal(1415))
+								Expect(renderedPage.Image.Bounds().Size().Y).To(Equal(2000))
 							})
 						})
 						Context("and the width being larger than the height", func() {
 							It("returns the right image, point to pixel ratio and resolution", func() {
-								pageSize, err := pdfium.RenderPageInPixels(&requests.RenderPageInPixels{
+								renderedPage, err := pdfium.RenderPageInPixels(&requests.RenderPageInPixels{
 									Page:   0,
 									Width:  4000,
 									Height: 2000,
 								})
+
 								Expect(err).To(BeNil())
-								Expect(pageSize).To(Equal(&responses.RenderPage{
-									PointToPixelRatio: 4.166666666666667,
+								Expect(renderedPage).To(Equal(&responses.RenderPage{
+									Image:             loadPrerenderedImage("./testdata/render_testpdf_pixels_4000x2000.gob"),
+									PointToPixelRatio: 2.375608084404265,
 								}))
 
-								Expect(pageSize.Image.Bounds().Size().X).To(Equal(827))
-								Expect(pageSize.Image.Bounds().Size().Y).To(Equal(1170))
+								Expect(renderedPage.Image.Bounds().Size().X).To(Equal(1415))
+								Expect(renderedPage.Image.Bounds().Size().Y).To(Equal(2000))
 							})
 						})
 
 						Context("and the height being larger than the width", func() {
 							It("returns the right image, point to pixel ratio and resolution", func() {
-								pageSize, err := pdfium.RenderPageInPixels(&requests.RenderPageInPixels{
+								renderedPage, err := pdfium.RenderPageInPixels(&requests.RenderPageInPixels{
 									Page:   0,
 									Width:  2000,
 									Height: 4000,
 								})
+
 								Expect(err).To(BeNil())
-								Expect(pageSize).To(Equal(&responses.RenderPage{
-									PointToPixelRatio: 4.166666666666667,
+								Expect(renderedPage).To(Equal(&responses.RenderPage{
+									Image:             loadPrerenderedImage("./testdata/render_testpdf_pixels_2000x4000.gob"),
+									PointToPixelRatio: 3.3597884547259587,
 								}))
 
-								Expect(pageSize.Image.Bounds().Size().X).To(Equal(827))
-								Expect(pageSize.Image.Bounds().Size().Y).To(Equal(1170))
+								Expect(renderedPage.Image.Bounds().Size().X).To(Equal(2000))
+								Expect(renderedPage.Image.Bounds().Size().Y).To(Equal(2829))
 							})
 						})
 					})
@@ -254,3 +239,31 @@ var _ = Describe("Render", func() {
 		})
 	})
 })
+
+func loadPrerenderedImage(path string) *image.RGBA {
+	preRender, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil
+	}
+
+	buf := bytes.NewBuffer(preRender)
+	dec := gob.NewDecoder(buf)
+
+	var preRenderImage image.RGBA
+	err = dec.Decode(&preRenderImage)
+	return &preRenderImage
+}
+
+func writePrerenderedImage(path string, image image.RGBA) error {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(&image); err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(path, buf.Bytes(), 0777); err != nil {
+		return err
+	}
+
+	return nil
+}
