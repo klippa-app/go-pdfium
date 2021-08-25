e031f8e8278a412bd58cbe533e25e82763bc1e35
Jeroen Bobbeldijk <jeroen@klippa.com>
2021-08-25 16:48:11 +0200

    Add pdfium implementation tests

--- pdfium/internal/subprocess/subprocess_test.go
+++ pdfium/internal/subprocess/subprocess_test.go
@@ -1,11 +1,71 @@
 package subprocess_test
 
 import (
-	"testing"
+	"io/ioutil"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/klippa-app/go-pdfium/pdfium/internal/subprocess"
+	"github.com/klippa-app/go-pdfium/pdfium/pdfium_errors"
+	"github.com/klippa-app/go-pdfium/pdfium/requests"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestStub(t *testing.T) {
-	assert.True(t, true, "This is good. Canary test passing")
-}
+var _ = Describe("Subprocess", func() {
+	pdfium := subprocess.Pdfium{}
+	AfterEach(func() {
+		pdfium.Close()
+	})
+
+	When("pinged", func() {
+		It("pongs", func() {
+			resp, err := pdfium.Ping()
+			Expect(err).To(BeNil())
+			Expect(resp).To(Equal("Pong"))
+		})
+	})
+
+	Context("a normal PDF file", func() {
+		pdfData, _ := ioutil.ReadFile("./testdata/test.pdf")
+		When("is opened", func() {
+			It("does not return an error", func() {
+				err := pdfium.OpenDocument(&requests.OpenDocument{
+					File: &pdfData,
+				})
+				Expect(err).To(BeNil())
+			})
+		})
+	})
+
+	Context("a password protected PDF file", func() {
+		pdfData, _ := ioutil.ReadFile("./testdata/password_test123.pdf")
+		When("is opened with no password", func() {
+			It("returns the password error", func() {
+				err := pdfium.OpenDocument(&requests.OpenDocument{
+					File: &pdfData,
+				})
+				Expect(err).To(Equal(pdfium_errors.ErrPassword))
+			})
+		})
+		When("is opened with the wrong password", func() {
+			It("returns the password error", func() {
+				wrongPassword := "test"
+				err := pdfium.OpenDocument(&requests.OpenDocument{
+					File:     &pdfData,
+					Password: &wrongPassword,
+				})
+				Expect(err).To(Equal(pdfium_errors.ErrPassword))
+			})
+		})
+		When("is opened with the correct password", func() {
+			It("does not return an error", func() {
+				pdfPassword := "test123"
+				err := pdfium.OpenDocument(&requests.OpenDocument{
+					File:     &pdfData,
+					Password: &pdfPassword,
+				})
+				Expect(err).To(BeNil())
+			})
+		})
+	})
+})
