826d147b3da30248c54cb1d4c6fd3c3fcc542ce0
Jeroen Bobbeldijk <jeroen@klippa.com>
2022-01-29 17:10:32 +0100

    Add internal tests

--- internal/implementation/implementation_test.go
+++ internal/implementation/implementation_test.go
@@ -2,23 +2,27 @@ package implementation_test
 
 import (
 	"github.com/klippa-app/go-pdfium/internal/implementation"
-
+	"github.com/klippa-app/go-pdfium/pdfium_single_threaded"
+	"github.com/klippa-app/go-pdfium/shared_tests"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Implementation", func() {
 	implementation.InitLibrary()
-	pdfium := implementation.Pdfium{}
-	AfterEach(func() {
-		pdfium.Close()
-	})
 
 	When("pinged", func() {
 		It("pongs", func() {
+			pdfium := implementation.Pdfium{}
 			resp, err := pdfium.Ping()
 			Expect(err).To(BeNil())
 			Expect(resp).To(Equal("Pong"))
+			pdfium.Close()
 		})
 	})
+
+	Pdfium := pdfium_single_threaded.Init()
+	shared_tests.RunRenderTests(Pdfium, "../../shared_tests", "internal")
+	shared_tests.RunDocumentTests(Pdfium, "../../shared_tests", "internal")
+	shared_tests.RunTextTests(Pdfium, "../../shared_tests", "internal")
 })
