f67bf837a1cb6bc93ac29be74f6d335b2f5fa054
Jeroen Bobbeldijk <jeroen@klippa.com>
2022-03-02 11:47:09 +0100

    fpdf_annot base

--- pdfium.go
+++ pdfium.go
@@ -1438,4 +1438,310 @@ type Pdfium interface {
 	FPDF_StructElement_GetChildAtIndex(request *requests.FPDF_StructElement_GetChildAtIndex) (*responses.FPDF_StructElement_GetChildAtIndex, error)
 
 	// End fpdf_structtree.h
+
+	// Start fpdf_annot.h
+
+	// FPDFAnnot_IsSupportedSubtype returns whether an annotation subtype is currently supported for creation.
+	// Experimental API.
+	FPDFAnnot_IsSupportedSubtype(request *requests.FPDFAnnot_IsSupportedSubtype) (*responses.FPDFAnnot_IsSupportedSubtype, error)
+
+	// FPDFPage_CreateAnnot creates an annotation in the given page of the given subtype. If the specified
+	// subtype is illegal or unsupported, then a new annotation will not be created.
+	// Must call FPDFPage_CloseAnnot() when the annotation returned by this
+	// function is no longer needed.
+	// Experimental API.
+	FPDFPage_CreateAnnot(request *requests.FPDFPage_CreateAnnot) (*responses.FPDFPage_CreateAnnot, error)
+
+	// FPDFPage_GetAnnotCount returns the number of annotations in a given page.
+	// Experimental API.
+	FPDFPage_GetAnnotCount(request *requests.FPDFPage_GetAnnotCount) (*responses.FPDFPage_GetAnnotCount, error)
+
+	// FPDFPage_GetAnnot returns annotation at the given page and index. Must call FPDFPage_CloseAnnot() when the
+	// annotation returned by this function is no longer needed.
+	// Experimental API.
+	FPDFPage_GetAnnot(request *requests.FPDFPage_GetAnnot) (*responses.FPDFPage_GetAnnot, error)
+
+	// FPDFPage_GetAnnotIndex returns the index of the given annotation in the given page. This is the opposite of
+	// FPDFPage_GetAnnot().
+	// Experimental API.
+	FPDFPage_GetAnnotIndex(request *requests.FPDFPage_GetAnnotIndex) (*responses.FPDFPage_GetAnnotIndex, error)
+
+	// FPDFPage_CloseAnnot closes an annotation. Must be called when the annotation returned by
+	// FPDFPage_CreateAnnot() or FPDFPage_GetAnnot() is no longer needed. This
+	// function does not remove the annotation from the document.
+	// Experimental API.
+	FPDFPage_CloseAnnot(request *requests.FPDFPage_CloseAnnot) (*responses.FPDFPage_CloseAnnot, error)
+
+	// FPDFPage_RemoveAnnot removes the annotation in the given page at the given index.
+	// Experimental API.
+	FPDFPage_RemoveAnnot(request *requests.FPDFPage_RemoveAnnot) (*responses.FPDFPage_RemoveAnnot, error)
+
+	// FPDFAnnot_GetSubtype returns the subtype of an annotation.
+	// Experimental API.
+	FPDFAnnot_GetSubtype(request *requests.FPDFAnnot_GetSubtype) (*responses.FPDFAnnot_GetSubtype, error)
+
+	// FPDFAnnot_IsObjectSupportedSubtype checks whether an annotation subtype is currently supported for object extraction,
+	// update, and removal.
+	// Experimental API.
+	FPDFAnnot_IsObjectSupportedSubtype(request *requests.FPDFAnnot_IsObjectSupportedSubtype) (*responses.FPDFAnnot_IsObjectSupportedSubtype, error)
+
+	// FPDFAnnot_UpdateObject updates the given object in the given annotation. The object must be in the annotation already and must have
+	// been retrieved by FPDFAnnot_GetObject(). Currently, only ink and stamp
+	// annotations are supported by this API. Also note that only path, image, and
+	///text objects have APIs for modification; see FPDFPath_*(), FPDFText_*(), and
+	// FPDFImageObj_*().
+	// Experimental API.
+	FPDFAnnot_UpdateObject(request *requests.FPDFAnnot_UpdateObject) (*responses.FPDFAnnot_UpdateObject, error)
+
+	// FPDFAnnot_AddInkStroke adds a new InkStroke, represented by an array of points, to the InkList of
+	// the annotation. The API creates an InkList if one doesn't already exist in the annotation.
+	// This API works only for ink annotations. Please refer to ISO 32000-1:2008
+	// spec, section 12.5.6.13.
+	// Experimental API.
+	FPDFAnnot_AddInkStroke(request *requests.FPDFAnnot_AddInkStroke) (*responses.FPDFAnnot_AddInkStroke, error)
+
+	// FPDFAnnot_RemoveInkList removes an InkList in the given annotation.
+	// This API works only for ink annotations.
+	// Experimental API.
+	FPDFAnnot_RemoveInkList(request *requests.FPDFAnnot_RemoveInkList) (*responses.FPDFAnnot_RemoveInkList, error)
+
+	// FPDFAnnot_AppendObject adds the given object to the given annotation. The object must have been created by
+	// FPDFPageObj_CreateNew{Path|Rect}() or FPDFPageObj_New{Text|Image}Obj(), and
+	// will be owned by the annotation. Note that an object cannot belong to more than one
+	// annotation. Currently, only ink and stamp annotations are supported by this API.
+	// Also note that only path, image, and text objects have APIs for creation.
+	// Experimental API.
+	FPDFAnnot_AppendObject(request *requests.FPDFAnnot_AppendObject) (*responses.FPDFAnnot_AppendObject, error)
+
+	// FPDFAnnot_GetObjectCount returns the total number of objects in the given annotation, including path objects, text
+	// objects, external objects, image objects, and shading objects.
+	// Experimental API.
+	FPDFAnnot_GetObjectCount(request *requests.FPDFAnnot_GetObjectCount) (*responses.FPDFAnnot_GetObjectCount, error)
+
+	// FPDFAnnot_GetObject returns the object in the given annotation at the given index.
+	// Experimental API.
+	FPDFAnnot_GetObject(request *requests.FPDFAnnot_GetObject) (*responses.FPDFAnnot_GetObject, error)
+
+	// FPDFAnnot_RemoveObject removes the object in the given annotation at the given index.
+	// Experimental API.
+	FPDFAnnot_RemoveObject(request *requests.FPDFAnnot_RemoveObject) (*responses.FPDFAnnot_RemoveObject, error)
+
+	// FPDFAnnot_SetColor sets the color of an annotation. Fails when called on annotations with
+	// appearance streams already defined; instead use
+	// FPDFPath_Set{Stroke|Fill}Color().
+	// Experimental API.
+	FPDFAnnot_SetColor(request *requests.FPDFAnnot_SetColor) (*responses.FPDFAnnot_SetColor, error)
+
+	// FPDFAnnot_GetColor returns the color of an annotation. If no color is specified, default to yellow
+	// for highlight annotation, black for all else. Fails when called on
+	// annotations with appearance streams already defined; instead use
+	// FPDFPath_Get{Stroke|Fill}Color().
+	// Experimental API.
+	FPDFAnnot_GetColor(request *requests.FPDFAnnot_GetColor) (*responses.FPDFAnnot_GetColor, error)
+
+	// FPDFAnnot_HasAttachmentPoints returns whether the annotation is of a type that has attachment points
+	// (i.e. quadpoints). Quadpoints are the vertices of the rectangle that
+	// encompasses the texts affected by the annotation. They provide the
+	// coordinates in the page where the annotation is attached. Only text markup
+	// annotations (i.e. highlight, strikeout, squiggly, and underline) and link
+	// annotations have quadpoints.
+	// Experimental API.
+	FPDFAnnot_HasAttachmentPoints(request *requests.FPDFAnnot_HasAttachmentPoints) (*responses.FPDFAnnot_HasAttachmentPoints, error)
+
+	// FPDFAnnot_SetAttachmentPoints replaces the attachment points (i.e. quadpoints) set of an annotation at
+	// the given quad index. This index needs to be within the result of
+	// FPDFAnnot_CountAttachmentPoints().
+	// If the annotation's appearance stream is defined and this annotation is of a
+	// type with quadpoints, then update the bounding box too if the new quadpoints
+	// define a bigger one.
+	// Experimental API.
+	FPDFAnnot_SetAttachmentPoints(request *requests.FPDFAnnot_SetAttachmentPoints) (*responses.FPDFAnnot_SetAttachmentPoints, error)
+
+	// FPDFAnnot_AppendAttachmentPoints appends to the list of attachment points (i.e. quadpoints) of an annotation.
+	// If the annotation's appearance stream is defined and this annotation is of a
+	// type with quadpoints, then update the bounding box too if the new quadpoints
+	// define a bigger one.
+	// Experimental API.
+	FPDFAnnot_AppendAttachmentPoints(request *requests.FPDFAnnot_AppendAttachmentPoints) (*responses.FPDFAnnot_AppendAttachmentPoints, error)
+
+	// FPDFAnnot_CountAttachmentPoints returns the number of sets of quadpoints of an annotation.
+	// Experimental API.
+	FPDFAnnot_CountAttachmentPoints(request *requests.FPDFAnnot_CountAttachmentPoints) (*responses.FPDFAnnot_CountAttachmentPoints, error)
+
+	// FPDFAnnot_GetAttachmentPoints returns the attachment points (i.e. quadpoints) of an annotation.
+	// Experimental API.
+	FPDFAnnot_GetAttachmentPoints(request *requests.FPDFAnnot_GetAttachmentPoints) (*responses.FPDFAnnot_GetAttachmentPoints, error)
+
+	// FPDFAnnot_SetRect sets the annotation rectangle defining the location of the annotation. If the
+	// annotation's appearance stream is defined and this annotation is of a type
+	// without quadpoints, then update the bounding box too if the new rectangle
+	// defines a bigger one.
+	// Experimental API.
+	FPDFAnnot_SetRect(request *requests.FPDFAnnot_SetRect) (*responses.FPDFAnnot_SetRect, error)
+
+	// FPDFAnnot_GetRect returns the annotation rectangle defining the location of the annotation.
+	// Experimental API.
+	FPDFAnnot_GetRect(request *requests.FPDFAnnot_GetRect) (*responses.FPDFAnnot_GetRect, error)
+
+	// FPDFAnnot_GetVertices returns the vertices of a polygon or polyline annotation.
+	// Experimental API.
+	FPDFAnnot_GetVertices(request *requests.FPDFAnnot_GetVertices) (*responses.FPDFAnnot_GetVertices, error)
+
+	// FPDFAnnot_GetInkListCount returns the number of paths in the ink list of an ink annotation.
+	// Experimental API.
+	FPDFAnnot_GetInkListCount(request *requests.FPDFAnnot_GetInkListCount) (*responses.FPDFAnnot_GetInkListCount, error)
+
+	// FPDFAnnot_GetInkListPath returns a path in the ink list of an ink annotation.
+	// Experimental API.
+	FPDFAnnot_GetInkListPath(request *requests.FPDFAnnot_GetInkListPath) (*responses.FPDFAnnot_GetInkListPath, error)
+
+	// FPDFAnnot_GetLine returns the starting and ending coordinates of a line annotation.
+	// Experimental API.
+	FPDFAnnot_GetLine(request *requests.FPDFAnnot_GetLine) (*responses.FPDFAnnot_GetLine, error)
+
+	// FPDFAnnot_SetBorder sets the characteristics of the annotation's border (rounded rectangle).
+	// Experimental API.
+	FPDFAnnot_SetBorder(request *requests.FPDFAnnot_SetBorder) (*responses.FPDFAnnot_SetBorder, error)
+
+	// FPDFAnnot_GetBorder returns the characteristics of the annotation's border (rounded rectangle).
+	// Experimental API.
+	FPDFAnnot_GetBorder(request *requests.FPDFAnnot_GetBorder) (*responses.FPDFAnnot_GetBorder, error)
+
+	// FPDFAnnot_HasKey checks whether the given annotation's dictionary has the given key as a key.
+	// Experimental API.
+	FPDFAnnot_HasKey(request *requests.FPDFAnnot_HasKey) (*responses.FPDFAnnot_HasKey, error)
+
+	// FPDFAnnot_GetValueType returns the type of the value corresponding to the given key the annotation's dictionary.
+	// Experimental API.
+	FPDFAnnot_GetValueType(request *requests.FPDFAnnot_GetValueType) (*responses.FPDFAnnot_GetValueType, error)
+
+	// FPDFAnnot_SetStringValue sets the string value corresponding to the given key in the annotations's dictionary,
+	// overwriting the existing value if any. The value type would be
+	// FPDF_OBJECT_STRING after this function call succeeds.
+	// Experimental API.
+	FPDFAnnot_SetStringValue(request *requests.FPDFAnnot_SetStringValue) (*responses.FPDFAnnot_SetStringValue, error)
+
+	// FPDFAnnot_GetStringValue returns the string value corresponding to the given key in the annotations's dictionary.
+	// Experimental API.
+	FPDFAnnot_GetStringValue(request *requests.FPDFAnnot_GetStringValue) (*responses.FPDFAnnot_GetStringValue, error)
+
+	// FPDFAnnot_GetNumberValue returns the float value corresponding to the given key in the annotations's dictionary.
+	// Experimental API.
+	FPDFAnnot_GetNumberValue(request *requests.FPDFAnnot_GetNumberValue) (*responses.FPDFAnnot_GetNumberValue, error)
+
+	// FPDFAnnot_SetAP sets the AP (appearance string) in annotations's dictionary for a given appearance mode.
+	// Experimental API.
+	FPDFAnnot_SetAP(request *requests.FPDFAnnot_SetAP) (*responses.FPDFAnnot_SetAP, error)
+
+	// FPDFAnnot_GetAP returns the AP (appearance string) from annotation's dictionary for a given
+	// appearance mode.
+	// Experimental API.
+	FPDFAnnot_GetAP(request *requests.FPDFAnnot_GetAP) (*responses.FPDFAnnot_GetAP, error)
+
+	// FPDFAnnot_GetLinkedAnnot returns the annotation corresponding to the given key in the annotations's dictionary. Common
+	// keys for linking annotations include "IRT" and "Popup". Must call
+	// FPDFPage_CloseAnnot() when the annotation returned by this function is no
+	// longer needed.
+	// Experimental API.
+	FPDFAnnot_GetLinkedAnnot(request *requests.FPDFAnnot_GetLinkedAnnot) (*responses.FPDFAnnot_GetLinkedAnnot, error)
+
+	// FPDFAnnot_GetFlags returns the annotation flags of the given annotation.
+	// Experimental API.
+	FPDFAnnot_GetFlags(request *requests.FPDFAnnot_GetFlags) (*responses.FPDFAnnot_GetFlags, error)
+
+	// FPDFAnnot_SetFlags sets the annotation flags of the given annotation.
+	// Experimental API.
+	FPDFAnnot_SetFlags(request *requests.FPDFAnnot_SetFlags) (*responses.FPDFAnnot_SetFlags, error)
+
+	// FPDFAnnot_GetFormFieldFlags returns the form field annotation flags of the given annotation.
+	// Experimental API.
+	FPDFAnnot_GetFormFieldFlags(request *requests.FPDFAnnot_GetFormFieldFlags) (*responses.FPDFAnnot_GetFormFieldFlags, error)
+
+	// FPDFAnnot_GetFormFieldAtPoint returns an interactive form annotation whose rectangle contains a given
+	// point on a page. Must call FPDFPage_CloseAnnot() when the annotation returned
+	// is no longer needed.
+	// Experimental API.
+	FPDFAnnot_GetFormFieldAtPoint(request *requests.FPDFAnnot_GetFormFieldAtPoint) (*responses.FPDFAnnot_GetFormFieldAtPoint, error)
+
+	// FPDFAnnot_GetFormFieldName returns the name of the given annotation, which is an interactive form annotation.
+	// Experimental API.
+	FPDFAnnot_GetFormFieldName(request *requests.FPDFAnnot_GetFormFieldName) (*responses.FPDFAnnot_GetFormFieldName, error)
+
+	// FPDFAnnot_GetFormFieldType returns the form field type of the given annotation, which is an interactive form annotation.
+	// Experimental API.
+	FPDFAnnot_GetFormFieldType(request *requests.FPDFAnnot_GetFormFieldType) (*responses.FPDFAnnot_GetFormFieldType, error)
+
+	// FPDFAnnot_GetFormFieldValue returns the value of the given annotation, which is an interactive form annotation.
+	// Experimental API.
+	FPDFAnnot_GetFormFieldValue(request *requests.FPDFAnnot_GetFormFieldValue) (*responses.FPDFAnnot_GetFormFieldValue, error)
+
+	// FPDFAnnot_GetOptionCount returns the number of options in the annotation's "Opt" dictionary. Intended for
+	// use with listbox and combobox widget annotations.
+	// Experimental API.
+	FPDFAnnot_GetOptionCount(request *requests.FPDFAnnot_GetOptionCount) (*responses.FPDFAnnot_GetOptionCount, error)
+
+	// FPDFAnnot_GetOptionLabel returns the string value for the label of the option at the given index in annotation's
+	// "Opt" dictionary. Intended for use with listbox and combobox widget
+	// annotations.
+	// Experimental API.
+	FPDFAnnot_GetOptionLabel(request *requests.FPDFAnnot_GetOptionLabel) (*responses.FPDFAnnot_GetOptionLabel, error)
+
+	// FPDFAnnot_IsOptionSelected returns whether or not the option at the given index in annotation's "Opt" dictionary
+	// is selected. Intended for use with listbox and combobox widget annotations.
+	// Experimental API.
+	FPDFAnnot_IsOptionSelected(request *requests.FPDFAnnot_IsOptionSelected) (*responses.FPDFAnnot_IsOptionSelected, error)
+
+	// FPDFAnnot_GetFontSize returns the float value of the font size for an annotation with variable text.
+	// If 0, the font is to be auto-sized: its size is computed as a function of
+	// the height of the annotation rectangle.
+	// Experimental API.
+	FPDFAnnot_GetFontSize(request *requests.FPDFAnnot_GetFontSize) (*responses.FPDFAnnot_GetFontSize, error)
+
+	// FPDFAnnot_IsChecked returns whether the given annotation is a form widget that is checked. Intended for use with
+	// checkbox and radio button widgets.
+	// Experimental API.
+	FPDFAnnot_IsChecked(request *requests.FPDFAnnot_IsChecked) (*responses.FPDFAnnot_IsChecked, error)
+
+	// FPDFAnnot_SetFocusableSubtypes returns the list of focusable annotation subtypes. Annotations of subtype
+	// FPDF_ANNOT_WIDGET are by default focusable. New subtypes set using this API
+	// will override the existing subtypes.
+	// Experimental API.
+	FPDFAnnot_SetFocusableSubtypes(request *requests.FPDFAnnot_SetFocusableSubtypes) (*responses.FPDFAnnot_SetFocusableSubtypes, error)
+
+	// FPDFAnnot_GetFocusableSubtypesCount returns the count of focusable annotation subtypes as set by host.
+	// Experimental API.
+	FPDFAnnot_GetFocusableSubtypesCount(request *requests.FPDFAnnot_GetFocusableSubtypesCount) (*responses.FPDFAnnot_GetFocusableSubtypesCount, error)
+
+	// FPDFAnnot_GetFocusableSubtypes returns the list of focusable annotation subtype as set by host.
+	// Experimental API.
+	FPDFAnnot_GetFocusableSubtypes(request *requests.FPDFAnnot_GetFocusableSubtypes) (*responses.FPDFAnnot_GetFocusableSubtypes, error)
+
+	// FPDFAnnot_GetLink returns FPDF_LINK object for the given annotation. Intended to use for link annotations.
+	// Experimental API.
+	FPDFAnnot_GetLink(request *requests.FPDFAnnot_GetLink) (*responses.FPDFAnnot_GetLink, error)
+
+	// FPDFAnnot_GetFormControlCount returns the count of annotations in the annotation's control group.
+	// A group of interactive form annotations is collectively called a form
+	// control group. Here, annotation, an interactive form annotation, should be
+	// either a radio button or a checkbox.
+	// Experimental API.
+	FPDFAnnot_GetFormControlCount(request *requests.FPDFAnnot_GetFormControlCount) (*responses.FPDFAnnot_GetFormControlCount, error)
+
+	// FPDFAnnot_GetFormControlIndex returns the index of the given annotation it's control group.
+	// A group of interactive form annotations is collectively called a form
+	// control group. Here, the annotation, an interactive form annotation, should be
+	// either a radio button or a checkbox.
+	// Experimental API.
+	FPDFAnnot_GetFormControlIndex(request *requests.FPDFAnnot_GetFormControlIndex) (*responses.FPDFAnnot_GetFormControlIndex, error)
+
+	// FPDFAnnot_GetFormFieldExportValue returns the export value of the given annotation which is an interactive form annotation.
+	// Intended for use with radio button and checkbox widget annotations.
+	// Experimental API.
+	FPDFAnnot_GetFormFieldExportValue(request *requests.FPDFAnnot_GetFormFieldExportValue) (*responses.FPDFAnnot_GetFormFieldExportValue, error)
+
+	// FPDFAnnot_SetURI adds a URI action to the given annotation, overwriting the existing action, if any.
+	// Experimental API.
+	FPDFAnnot_SetURI(request *requests.FPDFAnnot_SetURI) (*responses.FPDFAnnot_SetURI, error)
+
+	// End fpdf_annot.h
 }
