6de2b5511986f32a25e497ef2772cd6319f90259
Jeroen Bobbeldijk <jeroen@klippa.com>
2022-03-08 15:25:40 +0100

    Try to fix experimental flag in non-experimental build

--- multi_threaded/multi_threaded_test.go
+++ multi_threaded/multi_threaded_test.go
@@ -16,14 +16,22 @@ var _ = BeforeSuite(func() {
 	err := os.Setenv("TZ", "UTC")
 	Expect(err).To(BeNil())
 
+	args := []string{"run", "-exec", "env DYLD_LIBRARY_PATH=/opt/pdfium/lib"}
+	experimental := os.Getenv("IS_EXPERIMENTAL")
+	if experimental == "1" {
+		args = append(args, []string{"-tags", "pdfium_experimental"}...)
+	}
+
+	args = append(args, "../examples/multi_threaded/worker/main.go")
+
 	pool := multi_threaded.Init(multi_threaded.Config{
 		MinIdle:  1, // Makes sure that at least x workers are always available
 		MaxIdle:  1, // Makes sure that at most x workers are ever available
 		MaxTotal: 1, // Maxium amount of workers in total, allows the amount of workers to grow when needed, items between total max and idle max are automatically cleaned up, while idle workers are kept alive so they can be used directly.
 		Command: multi_threaded.Command{
-			BinPath:      "go",                                                                                                                                           // Only do this while developing, on production put the actual binary path in here. You should not want the Go runtime on production.
-			Args:         []string{"run", "-exec", "env DYLD_LIBRARY_PATH=/opt/pdfium/lib", "-tags", "pdfium_experimental", "../examples/multi_threaded/worker/main.go"}, // This is a reference to the worker package, this can be left empty when using a direct binary path.
-			StartTimeout: time.Minute * 15,                                                                                                                               // Some test environments are real slow.
+			BinPath:      "go",             // Only do this while developing, on production put the actual binary path in here. You should not want the Go runtime on production.
+			Args:         args,             // This is a reference to the worker package, this can be left empty when using a direct binary path.
+			StartTimeout: time.Minute * 15, // Some test environments are real slow.
 		},
 	})
 
