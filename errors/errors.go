41d43f343a298443aeb16ad9c00bbe5b603d6349
Jeroen Bobbeldijk <jeroen@klippa.com>
2023-09-20 16:30:49 +0200

    Add pdfium code to the errors

--- errors/errors.go
+++ errors/errors.go
@@ -3,13 +3,13 @@ package errors
 import "errors"
 
 var (
-	ErrSuccess                  = errors.New("success")
-	ErrUnknown                  = errors.New("unknown error")
-	ErrFile                     = errors.New("unable to read file")
-	ErrFormat                   = errors.New("incorrect format")
-	ErrPassword                 = errors.New("invalid password")
-	ErrSecurity                 = errors.New("invalid encryption")
-	ErrPage                     = errors.New("incorrect page")
+	ErrSuccess                  = errors.New("0: success")
+	ErrUnknown                  = errors.New("1: unknown error")
+	ErrFile                     = errors.New("2: unable to read file")
+	ErrFormat                   = errors.New("3: incorrect format")
+	ErrPassword                 = errors.New("4: invalid password")
+	ErrSecurity                 = errors.New("5: invalid encryption")
+	ErrPage                     = errors.New("6: incorrect page")
 	ErrUnexpected               = errors.New("unexpected error")
 	ErrExperimentalUnsupported  = errors.New("this functionality is only supported when using the pdfium_experimental build flag, see https://github.com/klippa-app/go-pdfium#experimental for more information")
 	ErrWindowsUnsupported       = errors.New("this functionality is Windows only")
